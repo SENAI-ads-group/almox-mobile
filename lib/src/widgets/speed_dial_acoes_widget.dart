ce5fb9c32db9d51b04ca10f5ffd89a5bc9818e40
Alexsander <alexsanderlaureanolopes@gmail.com>
2022-04-11 21:04:57 -0300

    botao_acao_requisicao_widget

--- lib/src/widgets/speed_dial_acoes_widget.dart
+++ lib/src/widgets/speed_dial_acoes_widget.dart
@@ -1,57 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class BotaoAcaoRequisicao extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("App"),
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: IconThemeData(size: 22.0),
+      onOpen: () => print('ABRINDO DIAL'),
+      onClose: () => print('ABRINDO DIAL'),
+      visible: true,
+      curve: Curves.bounceIn,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.ads_click_outlined, color: Colors.white),
+          backgroundColor: Colors.yellow,
+          onTap: () => print('ATENDER'),
+          label: 'ATENDER',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.yellow,
         ),
-        body: Container(
-          child: Center(
-            child: Text("Informe uma ação para a Requisição"),
-          ),
+        SpeedDialChild(
+          child: Icon(Icons.delivery_dining_sharp, color: Colors.white),
+          backgroundColor: Colors.green,
+          onTap: () => print('ENTREGAR'),
+          label: 'ENTREGAR',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.green,
         ),
-        floatingActionButton: SpeedDial(
-          animatedIcon: AnimatedIcons.menu_close,
-          animatedIconTheme: IconThemeData(size: 22.0),
-          onOpen: () => print('ABRINDO DIAL'),
-          onClose: () => print('ABRINDO DIAL'),
-          visible: true,
-          curve: Curves.bounceIn,
-          children: [
-            SpeedDialChild(
-              child: Icon(Icons.ads_click_outlined, color: Colors.white),
-              backgroundColor: Colors.yellow,
-              onTap: () => print('ATENDER'),
-              label: 'ATENDER',
-              labelStyle: TextStyle(fontWeight: FontWeight.w500),
-              labelBackgroundColor: Colors.yellow,
-            ),
-            SpeedDialChild(
-              child: Icon(Icons.delivery_dining_sharp, color: Colors.white),
-              backgroundColor: Colors.green,
-              onTap: () => print('ENTREGAR'),
-              label: 'ENTREGAR',
-              labelStyle: TextStyle(fontWeight: FontWeight.w500),
-              labelBackgroundColor: Colors.green,
-            ),
-            SpeedDialChild(
-              child: Icon(Icons.cancel_outlined, color: Colors.white),
-              backgroundColor: Colors.red,
-              onTap: () => print('CANCELAR'),
-              label: 'CANCELAR',
-              labelStyle: TextStyle(fontWeight: FontWeight.w500),
-              labelBackgroundColor: Colors.red,
-            ),
-          ],
+        SpeedDialChild(
+          child: Icon(Icons.cancel_outlined, color: Colors.white),
+          backgroundColor: Colors.red,
+          onTap: () => print('CANCELAR'),
+          label: 'CANCELAR',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.red,
         ),
-      ),
+      ],
     );
   }
 }

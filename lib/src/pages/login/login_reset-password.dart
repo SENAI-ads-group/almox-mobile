0ae88a2cd0360f45e953df5b55befa82154d3c53
Leonardo Ribeiro <leonardo@ericom.com.br>
2022-04-25 12:13:44 -0300

    Inserido validaçao de campos no Form
    	modified:   lib/src/pages/login/login_novo_acesso.dart
    	modified:   lib/src/pages/login/login_reset-password.dart
    	modified:   lib/src/pages/login/login_screen.dart
    	new file:   lib/src/validators/validators.dart
    	modified:   pubspec.lock
    	modified:   pubspec.yaml

--- lib/src/pages/login/login_reset-password.dart
+++ lib/src/pages/login/login_reset-password.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:validatorless/validatorless.dart';
 
 class ResetPassword extends StatefulWidget {
   const ResetPassword({Key? key}) : super(key: key);
-
-   
 
   @override
   State<ResetPassword> createState() => _ResetPasswordState();
 }
 
 class _ResetPasswordState extends State<ResetPassword> {
+  final _emailEC = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _emailEC.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,85 +32,95 @@ class _ResetPasswordState extends State<ResetPassword> {
       ),
       body: Container(
         padding: EdgeInsets.only(
-          top: 20,
+          top: 10,
           left: 40,
           right: 40,
-        
         ),
-        color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              width: 200,
-              height: 200,
-              child: Image.asset("assets/images/resetPassword.jpg"),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Esqueceu sua Senha?",
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.blue,
-                fontWeight: FontWeight.w500,
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset("assets/images/resetPassword.jpg"),
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Preecha o campo abaixo para enviarmos um E-mail com as instruções de recuperação.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.blue,
-                fontWeight: FontWeight.w400,
+              SizedBox(
+                height: 10,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-               TextFormField(
-               
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email_outlined),
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
+              Text(
+                "Esqueceu sua Senha?",
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
                 ),
+                textAlign: TextAlign.center,
               ),
-              style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 60,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                gradient: const LinearGradient(
-                    colors: [Colors.blue, Colors.lightBlueAccent]),
+              SizedBox(
+                height: 10,
               ),
-              child: MaterialButton(
-                onPressed: () {},
-                child: const Text(
-                  "Enviar",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
+              Text(
+                'Preecha o campo abaixo para enviarmos um E-mail com as instruções de recuperação.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                controller: _emailEC,
+                validator: Validatorless.multiple([
+                  Validatorless.required('Preecha o campo Email'),
+                  Validatorless.email('Email incorreto')
+                ]),
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email_outlined),
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  gradient: const LinearGradient(
+                      colors: [Colors.blue, Colors.blue]),
+                ),
+                child: MaterialButton(
+                  onPressed: () {
+                    var formValid = _formKey.currentState?.validate() ?? false;
+                    if (formValid) {}
+                  },
+                  child: const Text(
+                    "Enviar",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20,)
-          ],
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
         ),
       ),
     );
